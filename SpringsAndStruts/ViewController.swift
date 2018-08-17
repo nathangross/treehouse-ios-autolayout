a5b0a6febb42c8a4a978fd06199449f4991d7443
Nathan Gross <nathan@atomicrobot.io>
2018-08-17 10:45:01 -0400

    Initial Commit

---
+++ SpringsAndStruts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpringsAndStruts
+//
+//  Created by Nathan Gross on 8/17/18.
+//  Copyright © 2018 Nathan Gross. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
